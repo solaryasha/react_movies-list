87f32af19c4537d700f234be0b0a5195597c17e8
solaryasha <solaryasha@gmail.com>
2020-06-21 00:46:20 +0300

    Updates

---
+++ precache-manifest.165774b163fb975c81a6560ecd62dcb0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15f902f709015f61e5320ee4a6812be3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "74cc4768fab5abcf4163",
+    "url": "./static/css/2.c3343ac3.chunk.css"
+  },
+  {
+    "revision": "dc5f541eeaa99ba61cfd",
+    "url": "./static/css/main.8a2ebc01.chunk.css"
+  },
+  {
+    "revision": "74cc4768fab5abcf4163",
+    "url": "./static/js/2.a151ed51.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.a151ed51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc5f541eeaa99ba61cfd",
+    "url": "./static/js/main.8b212982.chunk.js"
+  },
+  {
+    "revision": "7859211cf5cc2a17445c",
+    "url": "./static/js/runtime-main.2d746ae6.js"
+  }
+]);
